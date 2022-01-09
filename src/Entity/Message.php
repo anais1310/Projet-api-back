96c773d6461b1400aae4056cd6460fbdb824bc64
anais1310 <72029239+anais1310@users.noreply.github.com>
2022-01-09 22:09:58 +0100

    Add files via upload

---
+++ src/Entity/Message.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\MessageRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+
+/**
+ * @ORM\Entity(repositoryClass=MessageRepository::class)
+ */
+#[ApiResource]
+class Message
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Groups({"messages"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"messages"})
+     */
+    private $Titre;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Groups({"messages"})
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"messages"})
+     */
+    private $message;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="messages")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): self
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
